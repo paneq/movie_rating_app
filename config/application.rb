85764ed1516503bf457b1fd01b61b93d0216c5b2
Jędrek Domański <jedrek.domanski@gmail.com>
2017-02-17 20:54:03 +0100

    SMTP settings to send reset password instructions

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module MovieRatingApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    ActionMailer::Base.smtp_settings = {
+    	:address 							=> 'smtp.gmail.com',
+    	:domain 							=> 'mail.google.com',
+    	:port 								=> 587,
+    	:user_name 						    => "filmratingapp@gmail.com", #ENV['GMAIL_USERNAME'],
+    	:password 			      			=> 'Jedrek123', #ENV['GMAIL_PASSWORD'],
+    	:authentication 		         	=> 'login',
+    	:enable_starttls_auto => true
+    }
   end
 end
