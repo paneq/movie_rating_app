8d5d37a88977301c29cad16c31018169635871dd
Jędrek Domański <jedrek.domanski@gmail.com>
2017-02-17 01:01:36 +0100

    Fixed not working form on Reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,6 @@ class Review < ApplicationRecord
 	belongs_to :movie
 	belongs_to :user
 
-	validates :rating, length: { maximum: 100,
-    				 too_long: "%{count} characters is the maximum allowed" }, presence: true
+	validates :rating,  presence: true
 	validates :comment, presence: true
 end
