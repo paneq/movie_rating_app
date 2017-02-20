002bdb2a2ff8c84432d0382751362ba23e92e827
Jędrek Domański <jedrek.domanski@gmail.com>
2017-02-20 11:32:02 +0100

    *Editing a review - doesn't show my review "number" - fixed *Removed unnecessary method in test_helpder.rb *Fixed images being saved to AWS S3 on DEV environment. Should be stored localy

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,4 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  def sample_image(filename = "Blade-runner.jpg") 
-    File.new("test/fixtures/#{filename}")
-  end
 end
