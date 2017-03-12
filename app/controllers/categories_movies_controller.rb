391f6bf04a2dd20ada2c2b84df23d2aefa13c32a
Jędrek Domański <jedrek.domanski@gmail.com>
2017-03-12 18:07:05 +0100

    Fixed indentations

--- app/controllers/categories_movies_controller.rb
+++ app/controllers/categories_movies_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesMoviesController < ApplicationController
   def index
-      @category = Category.find(params[:category_id])
-      @movies = Movie.where(category_id: @category.id).order("created_at DESC")
+    @category = Category.find(params[:category_id])
+    @movies = Movie.where(category_id: @category.id).order("created_at DESC")
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,48 +1,48 @@
 class MoviesController < ApplicationController
-	before_action :find_movie, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_user!, only: [:new, :edit]
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
 
-	def index
-		@movies = Movie.all.order("created_at DESC")
-	end
+  def index
+    @movies = Movie.all.order("created_at DESC")
+  end
 
-	def show
-		if @movie.reviews.blank?
-			@average_review = 0
-		else
-			@average_review = @movie.reviews.average(:rating).round(2)
-		end
-	end
+  def show
+    if @movie.reviews.blank?
+      @average_review = 0
+    else
+      @average_review = @movie.reviews.average(:rating).round(2)
+    end
+  end
 
-	def new
-		@movie = current_user.movies.build
-	end
+  def new
+    @movie = current_user.movies.build
+  end
 
-	def edit
-		@movie_categories = Category.all.map{ |c| [c.name, c.id] }
-	end
+  def edit
+    @movie_categories = Category.all.map{ |c| [c.name, c.id] }
+  end
 
-	def update
-		@movie.category_id = params[:category_id]
-		if @movie.update(movie_params)
-			redirect_to movie_path(@movie)
-		else
-			render 'edit'
-		end
-	end
+  def update
+    @movie.category_id = params[:category_id]
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@movie.destroy
-		redirect_to root_path
-	end
+  def destroy
+    @movie.destroy
+    redirect_to root_path
+  end
 
-	private
-		def movie_params
-			params.require(:movie).permit(:title, :description,
-						:movie_length, :director, :image, :category_id)
-		end
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :description,
+            :movie_length, :director, :image, :category_id)
+    end
 
-		def find_movie
-			@movie = Movie.find(params[:id])
-		end
-	end
+    def find_movie
+      @movie = Movie.find(params[:id])
+    end
+  end
