170be66f2523f58c833b35d0994d18b464580f83
Jędrek Domański <jedrek.domanski@gmail.com>
2017-03-12 17:50:55 +0100

    Added missing file

---
+++ app/controllers/create_review_controller.rb
@@ -0,0 +1,26 @@
+class CreateReviewController < ApplicationController
+  before_action :find_movie
+  def create
+    @review = current_user.reviews.build(review_params)
+    @review.movie_id = @movie.id
+    if params[:review][:rating].blank?
+      @review.rating = 0
+    end
+
+    if @review.save
+      @review.create_activity :create, owner: current_user
+      redirect_to movie_path(@movie)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:rating, :comment)   
+    end
+
+    def find_movie
+      @movie = Movie.find(params[:movie_id])
+    end
+end
