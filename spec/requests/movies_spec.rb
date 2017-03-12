d796c389ceafc2e45739df70aba44b0265de7def
Jędrek Domański <jedrek.domanski@gmail.com>
2017-03-12 23:07:23 +0100

    Improved feature specs so that they don't use models

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 describe 'Logged in user can add a new movie', :type => :feature do
-  it 'create a new movie' do
+  it 'creates a new movie' do
     user = FactoryGirl.create(:user)
-    movie = FactoryGirl.build(:movie)
     sign_in user
     visit root_path
     expect(page).to have_content('Log out')
     visit new_movie_path
+    find('#movie_category_id').find(:xpath, 'option[1]').select_option
+    fill_in 'Movie title', with: 'The Walking Dead'
+    fill_in 'Description', with: 'A group of survivors try to survive in the apocalyptic world overrun by zombies'
+    fill_in 'Director', with: 'Test director'
+    fill_in 'Movie length', with: 45
+    attach_file('Image', 'spec/support/The_Walking_Dead.jpg')
     click_button 'Create Movie'
-    expect { movie.save }.to change(Movie.all, :count).by(1)
+    visit root_path
+    page.find('.movie')['src'].should have_content 'The_Walking_Dead.jpg'
   end
 end
 
